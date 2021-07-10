433e513688403544d4a87942dbb63fae150e9529
pydevsp <pydevsp@gmail.com>
2021-07-11 01:11:15 +0530

    added manage user endpoint

--- app/user/urls.py
+++ app/user/urls.py
@@ -5,5 +5,7 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
-    path('token/', CreateTokenView.as_view(), name='token'),
+    # name field use in test url ==== TOKEN_URL = reverse('user:tokenss')
+    path('token/', CreateTokenView.as_view(), name='tokenss'),
+    path('me', ManageUserView.as_view(), name='me'),
 ]
