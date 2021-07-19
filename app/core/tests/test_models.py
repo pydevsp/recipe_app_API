937862d894ed2cf267e1393b24165ca68d0daa6c
pydevsp <pydevsp@gmail.com>
2021-07-20 03:03:08 +0530

    Added recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -64,3 +64,13 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='chicken masala',
+            time_minutes=15,
+            price=150.00
+        )
+        self.assertEqual(str(recipe), recipe.title)
